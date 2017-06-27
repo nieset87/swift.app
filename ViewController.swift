ac1b260ca5f1594778fa3804f6cbbf0eafd4d85a
Matthew <matt.nieset@yahoo.com>
2017-06-26 20:10:56 -0400

    no message

--- ViewController.swift
+++ ViewController.swift
@@ -18,7 +18,15 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: Any) {
         
+        let addition = false
+        
+        if addition {
+        
         theLabel.text = "Your answer is \(Double(text1.text!)! + Double(text2.text!)!)"
+        } else {
+        theLabel.text = "Your answer is \(Double(text1.text!)! - Double(text2.text!)!)"
+        
+        }
         
     }
     
